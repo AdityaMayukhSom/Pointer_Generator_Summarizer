2ae23be88018ddfb31255eb7e86fc44da82a29c8
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-25 20:50:23 +0530

    added running script

---
+++ run.ps1
@@ -0,0 +1,25 @@
+python main.py `
+    --max_enc_len=400 `
+    --max_dec_len=100 `
+    --max_dec_steps=120 `
+    --min_dec_steps=30 `
+    --batch_size=4 `
+    --beam_size=4 `
+    --vocab_size=50000 `
+    --embed_size=128 `
+    --enc_units=256 `
+    --dec_units=256 `
+    --attn_units=512 `
+    --learning_rate=0.15 `
+    --adagrad_init_acc=0.1 `
+    --max_grad_norm=0.8 `
+    --mode="train" `
+    --checkpoints_save_steps=5000 `
+    --max_steps=38000 `
+    --num_to_test=5 `
+    --max_num_to_eval=100 `
+    --vocab_path="./tfrecords/vocab" `
+    --data_dir="./tfrecords/data" `
+    --model_path="./checkpoint/ckpt-37000" `
+    --checkpoint_dir="./checkpoint/" `
+    --test_save_dir="./test_dir/"
