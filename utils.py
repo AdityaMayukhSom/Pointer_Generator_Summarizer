d700feea02a8b6551e39983b924d9d8d606c4a4a
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-08-26 13:04:38 +0530

    imports organized and formatting fixed

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
-import os
 import logging
+import tensorflow as tf
 
 
 def define_logger(log_file):
@@ -10,9 +9,7 @@ def define_logger(log_file):
     log.setLevel(logging.DEBUG)
 
     # create formatter and add it to the handlers
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     # create file handler which logs even debug messages
     fh = logging.FileHandler(log_file)
@@ -42,18 +39,14 @@ def _calc_final_dist(
 
     # Multiply vocab dists by p_gen and attention dists by (1-p_gen)
     vocab_dists = [p_gen * dist for (p_gen, dist) in zip(p_gens, vocab_dists)]
-    attn_dists = [
-        (1 - p_gen) * dist for (p_gen, dist) in zip(p_gens, attn_dists)
-    ]
+    attn_dists = [(1 - p_gen) * dist for (p_gen, dist) in zip(p_gens, attn_dists)]
 
     # Concatenate some zeros to each vocabulary dist, to hold the probabilities for in-article OOV words
     # the maximum (over the batch) size of the extended vocabulary
     extended_vsize = vocab_size + batch_oov_len
     extra_zeros = tf.zeros((batch_size, batch_oov_len))
     # list length max_dec_steps of shape (batch_size, extended_vsize)
-    vocab_dists_extended = [
-        tf.concat(axis=1, values=[dist, extra_zeros]) for dist in vocab_dists
-    ]
+    vocab_dists_extended = [tf.concat(axis=1, values=[dist, extra_zeros]) for dist in vocab_dists]
 
     # Project the values in the attention distributions onto the appropriate entries in the final distributions
     # This means that if a_i = 0.1 and the ith encoder word is w, and w has index 500 in the vocabulary, then we add 0.1 onto the 500th entry of the final distribution
@@ -69,18 +62,13 @@ def _calc_final_dist(
     indices = tf.stack((batch_nums, _enc_batch_extend_vocab), axis=2)
     shape = [batch_size, extended_vsize]
     # list length max_dec_steps (batch_size, extended_vsize)
-    attn_dists_projected = [
-        tf.scatter_nd(indices, copy_dist, shape) for copy_dist in attn_dists
-    ]
+    attn_dists_projected = [tf.scatter_nd(indices, copy_dist, shape) for copy_dist in attn_dists]
 
     # Add the vocab distributions and the copy distributions together to get the final distributions
     # final_dists is a list length max_dec_steps; each entry is a tensor shape (batch_size, extended_vsize) giving the final distribution for that decoder timestep
     # Note that for decoder timesteps and examples corresponding to a [PAD] token, this is junk - ignore.
     final_dists = [
-        vocab_dist + copy_dist
-        for (vocab_dist, copy_dist) in zip(
-            vocab_dists_extended, attn_dists_projected
-        )
+        vocab_dist + copy_dist for (vocab_dist, copy_dist) in zip(vocab_dists_extended, attn_dists_projected)
     ]
 
     return final_dists
