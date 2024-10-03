956dcc4dd4635cc041464c10fdf065c1e08fdcdf
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-10-03 10:56:13 +0530

    removed custom multihead attention

--- decoder.py
+++ decoder.py
@@ -2,97 +2,6 @@ import keras
 import tensorflow as tf
 
 
-class MultiHeadAttention(keras.layers.Layer):
-    def __init__(self, d_model, num_heads):
-        super(MultiHeadAttention, self).__init__()
-        self.num_heads = num_heads
-        self.d_model = d_model
-
-        assert d_model % self.num_heads == 0
-
-        self.depth = d_model // self.num_heads
-
-        self.wq = keras.layers.Dense(d_model)
-        self.wk = keras.layers.Dense(d_model)
-        self.wv = keras.layers.Dense(d_model)
-
-        self.dense = keras.layers.Dense(d_model)
-
-    def scaled_dot_product_attention(self, q, k, v, mask):
-        """Calculate the attention weights.
-        q, k, v must have matching leading dimensions.
-        k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
-        The mask has different shapes depending on its type(padding or look ahead)
-        but it must be broadcastable for addition.
-
-        Args:
-        q: query shape == (..., seq_len_q, depth)
-        k: key shape == (..., seq_len_k, depth)
-        v: value shape == (..., seq_len_v, depth_v)
-        mask: Float tensor with shape broadcastable
-            to (..., seq_len_q, seq_len_k). Defaults to None.
-
-        Returns:
-        output, attention_weights
-        """
-
-        # (..., seq_len_q, seq_len_k)
-        matmul_qk = tf.matmul(q, k, transpose_b=True)
-
-        # scale matmul_qk
-        dk = tf.cast(tf.shape(k)[-1], tf.float32)  # type: ignore
-        scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
-
-        # add the mask to the scaled tensor.
-        if mask is not None:
-            scaled_attention_logits += mask * -1e9
-
-        # softmax is normalized on the last axis (seq_len_k) so that the scores
-        # add up to 1. (..., seq_len_q, seq_len_k)
-        attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
-
-        # (..., seq_len_q, depth_v)
-        output = tf.matmul(attention_weights, v)
-
-        return output, attention_weights
-
-    def split_heads(self, x, batch_size):
-        """Split the last dimension into (num_heads, depth).
-        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
-        """
-        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
-        return tf.transpose(x, perm=[0, 2, 1, 3])
-
-    def call(self, v, k, q, mask):
-        batch_size = tf.shape(q)[0]  # type: ignore
-
-        q = self.wq(q)  # (batch_size, seq_len, d_model)
-        k = self.wk(k)  # (batch_size, seq_len, d_model)
-        v = self.wv(v)  # (batch_size, seq_len, d_model)
-
-        # (batch_size, num_heads, seq_len_q, depth)
-        q = self.split_heads(q, batch_size)
-        # (batch_size, num_heads, seq_len_k, depth)
-        k = self.split_heads(k, batch_size)
-        # (batch_size, num_heads, seq_len_v, depth)
-        v = self.split_heads(v, batch_size)
-
-        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
-        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
-        scaled_attention, attention_weights = self.scaled_dot_product_attention(q, k, v, mask)
-
-        # (batch_size, seq_len_q, num_heads, depth)
-        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
-
-        # (batch_size, seq_len_q, d_model)
-        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.d_model))
-
-        # (batch_size, seq_len_q, d_model)
-        output = self.dense(concat_attention)
-
-        return output, attention_weights
-
-
 class FeedForward(keras.layers.Layer):
     """
     A class that implements the feed-forward layer.
@@ -145,8 +54,8 @@ class DecoderLayer(keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
         super(DecoderLayer, self).__init__()
 
-        self.mha1 = MultiHeadAttention(d_model, num_heads)
-        self.mha2 = MultiHeadAttention(d_model, num_heads)
+        self.mha1 = keras.layers.MultiHeadAttention(num_heads, d_model, d_model, dropout=rate)
+        self.mha2 = keras.layers.MultiHeadAttention(num_heads, d_model, d_model, dropout=rate)
 
         self.ffn = FeedForward(d_model, dff)
 
@@ -154,27 +63,37 @@ class DecoderLayer(keras.layers.Layer):
         self.layernorm2 = keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm3 = keras.layers.LayerNormalization(epsilon=1e-6)
 
-        self.dropout1 = keras.layers.Dropout(rate)
-        self.dropout2 = keras.layers.Dropout(rate)
-        self.dropout3 = keras.layers.Dropout(rate)
+        self.dropout = keras.layers.Dropout(rate)
 
-    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+    def call(self, x, enc_output, training: bool, look_ahead_mask, padding_mask):
         # enc_output.shape == (batch_size, input_seq_len, d_model)
 
         # (batch_size, target_seq_len, d_model)
-        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
-        attn1 = self.dropout1(attn1, training=training)
+        attn1, attn_weights_block1 = self.mha1(
+            query=x,
+            key=x,
+            value=x,
+            attention_mask=look_ahead_mask,
+            training=training,
+            return_attention_scores=True,
+        )
         out1 = self.layernorm1(attn1 + x)
 
         # (batch_size, target_seq_len, d_model)
-        attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask)
-        attn2 = self.dropout2(attn2, training=training)
+        attn2, attn_weights_block2 = self.mha2(
+            query=out1,
+            key=enc_output,
+            value=enc_output,
+            attention_mask=padding_mask,
+            training=training,
+            return_attention_scores=True,
+        )
         # (batch_size, target_seq_len, d_model)
         out2 = self.layernorm2(attn2 + out1)
 
         # (batch_size, target_seq_len, d_model)
         ffn_output = self.ffn(out2)
-        ffn_output = self.dropout3(ffn_output, training=training)
+        ffn_output = self.dropout(ffn_output, training=training)
         # (batch_size, target_seq_len, d_model)
         out3 = self.layernorm3(ffn_output + out2)
 
